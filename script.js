910a5ffb882071053bb433c156fc3d50a6a9bdb0
Nikita27277 <nikitathakur092006@gmail.com>
2025-10-19 11:40:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    const li = document.createElement("li");
+    li.textContent = taskText;
+    document.getElementById("taskList").appendChild(li);
+    taskInput.value = ""; // Clear input
+  }
+}
